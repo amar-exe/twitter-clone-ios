396ea0c87fadd434edc782eb6a32820b2518eb20
Amar Fazlić <73897903+amar-exe@users.noreply.github.com>
2023-01-31 09:30:17 +0100

    Profile header CollectionView

---
+++ View/ProfileFilterView.swift
@@ -0,0 +1,63 @@
+//
+//  ProfileFilterView.swift
+//  TwitterClone
+//
+//  Created by Amar Fazlic on 31. 1. 2023..
+//
+
+import UIKit
+
+class ProfileFilterView: UIView {
+    
+    //    MARK: Properties
+    
+    lazy var collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        let cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        cv.backgroundColor = .white
+        cv.delegate = self
+        cv.dataSource = self
+        return cv
+    }()
+    
+    //    MARK: Lifecycle
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        collectionView.register(ProfileFilterCell.self, forCellWithReuseIdentifier: ProfileFilterCell.reuseIdentifier)
+        
+        addSubview(collectionView)
+        collectionView.addConstraintsToFillView(self)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
+
+extension ProfileFilterView: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 3
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ProfileFilterCell.reuseIdentifier, for: indexPath) as! ProfileFilterCell
+        return cell
+    }
+}
+
+extension ProfileFilterView: UICollectionViewDelegate {
+
+}
+
+extension ProfileFilterView: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: frame.width / 3, height: frame.height)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+}
