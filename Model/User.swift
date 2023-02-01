d4b10d7f26070b12b7f4b3e766038933907d47fd
Amar Fazlić <73897903+amar-exe@users.noreply.github.com>
2023-02-01 13:31:34 +0100

    updating user following/follower stats

--- Model/User.swift
+++ Model/User.swift
@@ -15,6 +15,7 @@ struct User {
     var profileImageUrl: URL?
     let uid: String
     var isFollowed = false
+    var stats: UserRelationStats?
     
     var isCurrentUser: Bool {
         return Auth.auth().currentUser?.uid == uid
@@ -34,4 +35,11 @@ struct User {
         
         
     }
+}
+
+struct UserRelationStats {
+    let followers: Int
+    let following: Int
+    
+    
 }
