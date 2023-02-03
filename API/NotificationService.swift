9b44ed9fdd3de0e6d388e46dce65328a1357ab10
Amar Fazlić <73897903+amar-exe@users.noreply.github.com>
2023-02-03 11:14:12 +0100

    like notification

---
+++ API/NotificationService.swift
@@ -0,0 +1,27 @@
+//
+//  NotificationService.swift
+//  TwitterClone
+//
+//  Created by Amar Fazlic on 3. 2. 2023..
+//
+
+import Firebase
+
+struct NotificationService {
+    static let shared = NotificationService()
+    
+    func uploadNotification(type: NotificationType, tweet: Tweet? = nil ) {
+        guard let uid = Auth.auth().currentUser?.uid else { return }
+        
+        var values: [String : Any] = ["timestamp" : Int(NSDate().timeIntervalSince1970),
+                                      "uid" : uid,
+                                      "type" : type.rawValue]
+        
+        if let tweet = tweet {
+            values["tweetID"] = tweet.tweetID
+            REF_NOTIFICATIONS.child(tweet.user.uid).childByAutoId().updateChildValues(values)
+        } else {
+            
+        }
+    }
+}
